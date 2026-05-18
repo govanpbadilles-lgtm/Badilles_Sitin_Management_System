e4c18c5286e051fbee4ed9873a8d51e63254a6a3
govanpbadilles-lgtm <govanpbadilles@gmail.com>
2026-05-18 22:14:07 +0800

    add laboratory rule and others

--- app.py
+++ app.py
@@ -1304,5 +1304,5 @@ def get_lab_software():
 
 
 # =======================================================
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
